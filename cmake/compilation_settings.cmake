8c800a0d583e2d27260dfa260710bc0eaee09c01
Alvaro Acebedo <metakrtx@gmail.com>
2023-07-23 20:17:10 +0200

    fix: some parameters relocation in CMake configuration

--- cmake/compilation_settings.cmake
+++ cmake/compilation_settings.cmake
@@ -2,6 +2,12 @@
 # Now it is ensured -std::c++xx and prevented the use of non-portable compiler extensions
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+# Require out-of-source builds
+file(TO_CMAKE_PATH "${PROJECT_BINARY_DIR}/CMakeLists.txt" LOC_PATH)
+if (EXISTS "${LOC_PATH}")
+    message(FATAL_ERROR "You cannot build in a source directory (or any directory with a CMakeLists.txt file). Please use a build subdirectory instead. Feel free to remove CMakeCache.txt and CMakeFiles.")
+endif()
+
 # Support for folders in IDEs
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
